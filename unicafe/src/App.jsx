a6ee6a25e953d72124eb716280c5cc6a5955c44d
Dayesoro <dayesoro@gmail.com>
2024-04-04 18:23:02 +0100

    Exercise 1.7 solution
    unicafe step 2
    
    added more statistics to the application;total number of feedback, average score(good:1, neutral:0, bad:-1) and positive feedback percentage

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -17,13 +17,21 @@ const Button = ({ onPress, text }) => {
 }
 
 
-const Statistic = ({ text, score }) => {
+const Statistic = (props) => {
+  if (props.text === 'positive') {
+    return (
+      <div>
+        {props.text} {props.score} %
+      </div>
+    )
+  }
   return (
     <div>
-      {text} {score}
+      {props.text} {props.score}
     </div>
   )
 }
+
 
 
 
@@ -32,20 +40,43 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+
 
   const handleGoodClick = () => {
-    const newClick = good + 1;
-    setGood(newClick)
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    const updatedAll = all + 1
+    setAll(updatedAll)
+    const averageScore = (updatedGood * 1 + neutral * 0 + bad * -1) / updatedAll || 0; // Handle division by zero
+    setAverage(averageScore)
+    const positivePercentage = (updatedGood / updatedAll) * 100 || 0; // Calculate positive feedback percentage
+    setPositive(positivePercentage)
   }
 
   const handleNeutralClick = () => {
-    const newClick = neutral + 1;
-    setNeutral(newClick)
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    const updatedAll = all + 1
+    setAll(updatedAll)
+    const averageScore = (good * 1 + updatedNeutral * 0 + bad * -1) / updatedAll || 0; // Handle division by zero
+    setAverage(averageScore)
+    const positivePercentage = (good / updatedAll) * 100 || 0; // Calculate positive feedback percentage
+    setPositive(positivePercentage)
   }
 
   const handleBadClick = () => {
-    const newClick = bad + 1;
-    setBad(newClick)
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    const updatedAll = all + 1
+    setAll(updatedAll)
+    const averageScore = (good * 1 + neutral * 0 + updatedBad * -1) / updatedAll || 0; // Handle division by zero
+    setAverage(averageScore)
+    const positivePercentage = (good / updatedAll) * 100 || 0; // Calculate positive feedback percentage
+    setPositive(positivePercentage)
   }
 
 
@@ -59,6 +90,10 @@ const App = () => {
       <Statistic text='good' score={good} />
       <Statistic text='neutral' score={neutral} />
       <Statistic text='bad' score={bad} />
+      <Statistic text='all' score={all} />
+      <Statistic text='average' score={average} />
+      <Statistic text='positive' score={positive} />
+
     </div>
   )
 }
