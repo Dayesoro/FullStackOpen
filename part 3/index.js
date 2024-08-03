85538cbb841a7a10dcb5059d028304ed0419f665
Dayesoro <dayesoro@gmail.com>
2024-08-03 20:35:31 +0100

    Exercise 3.2
     The Phonebook backend step 2
    
     new Route implemented to show time of request and how many entries are in the phonebook

--- part 3/index.js
+++ part 3/index.js
@@ -28,6 +28,13 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    const phoneBookSize = persons.length
+    const requestTime = new Date()
+    response.send(`Phonebook has info for ${phoneBookSize} people <br/><br/> 
+        ${requestTime}`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
