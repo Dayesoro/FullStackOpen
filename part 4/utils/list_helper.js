69db760bc8a720108259c398a9721ae176d4c5d6
Dayesoro <dayesoro@gmail.com>
2025-01-02 23:32:58 +0000

    Exercises 4.3 - 4.5
    Helper Functions and Unit Tests, step 1-3

--- part 4/utils/list_helper.js
+++ part 4/utils/list_helper.js
@@ -1,7 +1,42 @@
-const dummy = (blogs) => {
-  return 1
+const dummy= (blogs) => {
+return 1
 }
 
-module.exports = {
-  dummy
+const totalLikes= (blogs) => {
+const reducer= (sum, blog) => {
+return sum + blog.likes
+ }
+
+return blogs.length===0
+?0
+: blogs.reduce(reducer, 0) 
+}
+
+const favouriteBlog= (blogs) => {
+const reducer= (max, blog) => {
+return blog.likes > max.likes ? blog : max
+ }
+
+if (blogs.length===0) {
+return null;
+ }
+
+const mostLikedBlog= blogs.reduce(reducer, blogs[0]);
+return {
+ title: mostLikedBlog.title,
+ author: mostLikedBlog.author,
+ likes: mostLikedBlog.likes,
+ };
+
+}
+
+const mostBlogs= (blogs) => {
+if (blogs.length===0) {
+return null;
+ }
+
+}
+
+module.exports= {
+ dummy, totalLikes, favouriteBlog
 }
